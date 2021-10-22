dbb782ba007ebb3dddcec557c437c610609286c8
amandena <manda.m.johnson@gmail.com>
2021-10-21 23:14:02 -0400

    imported Client and created temporary client variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Client from 'shopify-buy'
+
+const client = Client.buildClient({
+  storefrontAccessToken: 'your-access-token',
+  domain: 'your-shopify-url.myshopify.com'
+})
 
 ReactDOM.render(
   <React.StrictMode>
