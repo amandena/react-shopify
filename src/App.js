f7217d576deb4f510f486edd807cef6c68539c39
amandena <manda.m.johnson@gmail.com>
2021-10-22 20:37:41 -0400

    converted app to a class component and added cart import and constructor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react'
 import './App.css'
+import Cart from './components/Cart'
 
-function App() {
-  return (
-    <div className='App'>
-      Shopify App
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this)
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this)
+    this.handleCartClose = this.handleCartClose.bind(this)
+  }
+  render() {
+    return (
+      <div className='App'>
+        Shopify App
+      </div>
+    )
+  }
 }
 
 export default App
