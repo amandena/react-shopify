cc3e0d52b71b265679a496328140ef303b5b9d21
amandena <manda.m.johnson@gmail.com>
2021-10-27 18:54:18 -0400

    added more dispatches in index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import Client from 'shopify-buy'
 import { Provider } from 'react-redux'
 import store from './store'
+import App from './App'
+import Client from 'shopify-buy'
 
 const client = Client.buildClient({
   storefrontAccessToken: 'your-access-token',
   domain: 'your-shopify-url.myshopify.com'
 })
+
 store.dispatch({type: 'CLIENT_CREATED', payload: client})
+client.product.fetchAll().then((res) => {
+  store.dispatch({type: 'PRODUCTS_FOUND', payload: res});
+})
+client.checkout.create().then((res) => {
+  store.dispatch({type: 'CHECKOUT_FOUND', payload: res});
+})
+client.shop.fetchInfo().then((res) => {
+  store.dispatch({type: 'SHOP_FOUND', payload: res});
+})
 
 ReactDOM.render(
   <React.StrictMode>
