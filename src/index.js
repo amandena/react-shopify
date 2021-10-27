9bd36fefbd31e050761b4ecfa22c4c22aa948892
amandena <manda.m.johnson@gmail.com>
2021-10-26 23:21:05 -0400

    imported provider and store into index

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Client from 'shopify-buy'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const client = Client.buildClient({
   storefrontAccessToken: 'your-access-token',
   domain: 'your-shopify-url.myshopify.com'
 })
+store.dispatch({type: 'CLIENT_CREATED', payload: client})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <IntlProvider locale={locale} messages={flattenMessages(messages[locale.substring(0, 2)])}>
+        <App locale={locale}/>
+      </IntlProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
