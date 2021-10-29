dedd5c87630d46b42673d6565bfb055a1f9e5889
amandena <manda.m.johnson@gmail.com>
2021-10-28 23:13:39 -0400

    added cart handlers

--- src/App.js
+++ src/App.js
@@ -11,6 +11,32 @@ class App extends React.Component {
     this.removeLineItemInCart = this.removeLineItemInCart.bind(this)
     this.handleCartClose = this.handleCartClose.bind(this)
   }
+
+  updateQuantityInCart(lineItemId, quantity) {
+    const state = store.getState()
+    const checkoutId = state.checkout.id
+    const lineItemsToUpdate = [{id: lineItemId, quantity: parseInt(quantity, 10)}]
+    state.client.checkout.updateLineItems(checkoutId, lineItemsToUpdate).then(res => {
+      store.dispatch({type: 'UPDATE_QUANTITY_IN_CART', payload: {checkout: res}})
+    })
+  }
+
+  removeLineItemInCart(lineItemId) {
+    const state = store.getState()
+    const checkoutId = state.checkout.id
+    state.client.checkout.removeLineItems(checkoutId, [lineItemId]).then(res => {
+      store.dispatch({type: 'REMOVE_LINE_ITEM_IN_CART', payload: {checkout: res}})
+    })
+  }
+
+  handleCartClose() {
+    store.dispatch({type: 'CLOSE_CART'})
+  }
+
+  handleCartOpen() {
+    store.dispatch({type: 'OPEN_CART'})
+  }
+
   render() {
     const state = store.getState()
     return (
